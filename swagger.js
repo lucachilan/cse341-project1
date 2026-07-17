9c02bcbd0809c935d6c56254b72181566ea76ab4
Luca Chilan <lucachilan@gmail.com>
2026-07-17 04:05:16 -0300

    swagger config

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'Contacts API',
         description: 'Contacts Api'
     },
-    host: 'localhost:3000',
-    schemes: ['http', 'https']
+    host: 'https://cse341-o4b6.onrender.com',
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
